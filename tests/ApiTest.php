002f2fe3f70bd48929f744eaee8382324ffbba2b
Juanchi36 <juanmartingon@hotmail.com>
2019-11-01 19:30:52 -0300

    Adding travis

---
+++ tests/ApiTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class ApiTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testRoutesResponses()
+    {
+        $this->get('/games');
+        $this->assertResponseStatus(200);
+        $this->get('/game?id=2');
+        $this->assertResponseStatus(200);
+    }
+}
